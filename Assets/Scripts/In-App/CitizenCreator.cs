3be9292b456c06e523fe81a44c7c37b3ef80c2ce
llit0 <llit0show@gmail.com>
2021-03-03 19:34:05 +0800

    BOOOBA FEEEEET

---
+++ Assets/Scripts/In-App/CitizenCreator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CitizenCreator : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject citizen;
+
+    public void createCitizens()
+    {
+        
+    }
+}
