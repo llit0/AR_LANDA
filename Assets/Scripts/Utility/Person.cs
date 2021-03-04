5f3cd8be85b4c8766b3f2fa2e2dd138487abec85
llit0 <llit0show@gmail.com>
2021-03-04 16:09:37 +0800

    c

--- Assets/Scripts/Utility/Person.cs
+++ Assets/Scripts/Utility/Person.cs
@@ -1,6 +1,6 @@
 ﻿public abstract class Person
 {
    public int id;
-   public int[] position = new int[2]; 
-   public int[] velocity = new int[2];
+   public readonly int[] position = new int[2]; 
+   public readonly int[] velocity = new int[2];
 }
