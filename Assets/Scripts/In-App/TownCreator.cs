81dda6a572be5ca732e7fe726cd19594f88bf8c5
llit0 <llit0show@gmail.com>
2021-03-04 17:01:57 +0800

    Town Creator

--- Assets/Scripts/In-App/TownCreator.cs
+++ Assets/Scripts/In-App/TownCreator.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class TownCreator : MonoBehaviour
 {
@@ -10,13 +11,35 @@ public class TownCreator : MonoBehaviour
     private GameObject[] workBuildings;
     [SerializeField]
     private GameObject[] entertainBuildings;
-    
+
+
+    private Vector3 convertPosition(Building building)
+    {
+        const float halfCellSize = 1000f / 32f;
+        
+        Vector3 position = new Vector3
+        (
+            halfCellSize + halfCellSize * building.position[0], 
+            0f, 
+            halfCellSize + halfCellSize * building.position[1]
+        );
+        return position;
+    }
     private void createBuildings()
     {
+        Dictionary<string, GameObject[]> buildingArrays = new Dictionary<string, GameObject[]>()
+        {
+            {"home", homeBuildings},
+            {"work", workBuildings},
+            {"entertainment", entertainBuildings}
+        };
         List<Building> buildings = ServerConnection.town.data.buildings;
         foreach (Building building in buildings)
         {
-            
+            GameObject[] array = buildingArrays[building.type];
+            GameObject toInstantiate = array[Random.Range(0, array.Length)];
+            Vector3 buildingPos = convertPosition(building);
+            Instantiate(toInstantiate, buildingPos, Quaternion.identity, this.transform);
         }
     }
 }
